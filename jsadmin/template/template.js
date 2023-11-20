c9ac374d6c4ce77869f91846de17e350d3ed35ae
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-20 16:05:36 +0700

    delete

--- jsadmin/template/template.js
+++
@@ -1,9 +0,0 @@
-// template js
-
-//url register
-// export let URLPost = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/registerpakarbi`
-
-//url login 
-export let URLLogin = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/loginadminpakarbi`
-
-export let token = 'token';
