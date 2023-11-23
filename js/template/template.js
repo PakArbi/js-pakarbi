22466985709aa71c0b4738fbb59d0cc6d052ef25
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-23 21:35:01 +0700

    ubah kembali yang awal

--- js/template/template.js
+++ js/template/template.js
@@ -1,9 +1,9 @@
 // template js
 
 //url register
-// export let URLPost = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/registerpakarbi`
+export let URLPost = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/registerpakarbi`
 
 //url login 
-export let URLLogin = `https://asia-southeast2-pakarbi.cloudfunctions.net/loginpakarbi`
+export let URLLogin = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/loginpakarbi`
 
 export let token = 'token';
