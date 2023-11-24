116411bce0b76fa6de9f65c7dc916ff6b4083f9b
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-24 13:18:41 +0700

    update config

--- js/config/config.js
+++ js/config/config.js
@@ -33,12 +33,18 @@ export function GetDataForm() {
 
 // post login
 export function PostLogin() {
+    const username = document.getElementById("username").value;
     const npm = document.getElementById("npm").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     const passwordhash = document.getElementById("passwordhash").value;
     const role = document.getElementById("role").value;
 
     const data = {
+        username: username,
         npm: npm,
+        email: email,
+        password: password,
         passwordhash: passwordhash,
         role: role
     };
