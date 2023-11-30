82b7e23fb831e2fbb6fb79adff4956755ac35bf3
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-30 23:38:49 +0700

    update

--- jsparkiran/controller/controllerParkiran.js
+++ jsparkiran/controller/controllerParkiran.js
@@ -1,63 +1,13 @@
-// Include your getTokenFromCookies function here
+import {postWithBearer} from "https://jscroot.github.io/api/croot.js";
+import {GetDataForm,ResponsePost} from "../config/config.js";
+import {token,URLPost} from "../template/template.js";
 
-const getTokenFromCookies = (cookieName) => {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        if (name === cookieName) {
-            return value;
-        }
-    }
-    return null;
-};
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
 
-// Your other functions remain the same
-
-const insertParkiran = async (event) => {
-    event.preventDefault();
-
-    const token = getTokenFromCookies('Login');
-
-    if (!token) {
-        alert("Header Login Not Found");
-        return;
-    }
-
-    const targetURL = 'https://asia-southeast2-pakarbi.cloudfunctions.net/postparkiran';
-
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-    myHeaders.append('Content-Type', 'application/json');
-
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: JSON.stringify({
-            parkiranid: document.getElementById('parkiranid').value,
-            nama: document.getElementById('nama').value,
-            npm: document.getElementById('npm').value,
-            jurusan: document.getElementById('jurusan').value,
-            namakendaraan: document.getElementById('namakendaraan').value,
-            nomorkendaraan: document.getElementById('nomorkendaraan').value,
-            jeniskendaraan: document.getElementById('jeniskendaraan').value,
-        }),
-        redirect: 'follow',
-    };
-
-    try {
-        const response = await fetch(targetURL, requestOptions);
-        const data = await response.json();
-
-        if (data.status === false) {
-            alert(data.message);
-        } else {
-            alert("Employee data inserted successfully!");
-            // Optionally, you can reset the form or perform other actions.
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-};
-
-// Attach the insertEmployee function to the form's submit event
-document.getElementById('formparkiran').addEventListener('submit', insertParkiran);
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let data = GetDataForm();
+        postWithBearer(URLPost, token, data, ResponsePost)
+    });
+});
