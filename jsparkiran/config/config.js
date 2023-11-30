619676d37dae4a13a3272252cdf3f0f183967c8a
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-30 17:01:00 +0700

    add config js parkiran

---
+++ jsparkiran/config/config.js
@@ -0,0 +1,77 @@
+import {
+    setCookieWithExpireHour
+} from 'https://jscroot.github.io/cookie/croot.js';
+
+//token api
+export function getTokenFromAPI() {
+    const tokenUrl = "https://asia-southeast2-pakarbi.cloudfunctions.net/loginpakarbi";
+    fetch(tokenUrl)
+        .then(response => response.json())
+        .then(tokenData => {
+            if (tokenData.token) {
+                userToken = tokenData.token;
+                console.log('Token dari API:', userToken);
+            }
+        })
+        .catch(error => console.error('Gagal mengambil token:', error));
+}
+
+//get data 
+export function GetDataForm() {
+    const username = document.querySelector("#username").value;
+    const npm = document.querySelector("#npm").value;
+    const password = document.querySelector("#password").value;
+    const passwordhash = document.querySelector("#passwordhash").value;
+    const email = document.querySelector("#email").value;
+    const role = document.querySelector("#role").value;
+
+    const data = {
+        username: username,
+        npm: npm,
+        password: password,
+        passwordhash: passwordhash,
+        email: email,
+        role: role
+    };
+    return data
+}
+
+// post login
+export function PostLogin() {
+    const npm = document.getElementById("npm").value;
+    const passwordhash = document.getElementById("passwordhash").value;
+    const role = document.getElementById("role").value;
+
+    const data = {
+        npm: npm,
+        passwordhash: passwordhash,
+        role: role
+    };
+    return data;
+}
+
+// alert post 
+export function AlertPost(value) {
+    alert(value.message + "\nRegistrasi Berhasil")
+    window.location.href = "login.html"
+}
+
+// response post login
+function ResponsePostLogin(response) {
+    if (response && response.token) {
+        console.log('Token User:', response.token);
+        setCookieWithExpireHour('Token Login User', response.token, 2);
+        window.location.href = 'https://pakarbi.vaidiq.cloud/pages/dashboard.html';
+        alert("Selamat Datang")
+    } else {
+        alert('Login gagal. Silakan coba lagi.');
+    }
+}
+
+
+export function ResponsePost(result) {
+    AlertPost(result);
+}
+export function ResponseLogin(result) {
+    ResponsePostLogin(result)
+}
