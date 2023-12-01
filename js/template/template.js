fe10ea9079a44b85cdce473dcbbc6b468654399c
Farhan Rizki <farhanrizki101010@gmail.com>
2023-12-01 21:59:54 +0700

    update

--- js/template/template.js
+++ js/template/template.js
@@ -1,12 +1,12 @@
 // template js
 
 //url register
-export let URLPost = `https://us-central1-pakarbi.cloudfunctions.net/registerpakarbi`
+export let URLPost = `https://asia-southeast2-pakarbi.cloudfunctions.net/register`
 
 //url login 
-export let URLLogin = `https://asia-southeast2-pakarbi.cloudfunctions.net/loginpakarbi`
+export let URLLogin = `https://asia-southeast2-pakarbi.cloudfunctions.net/loginnew`
 
 //url login 
-export let URLLoginEmail = `https://asia-southeast2-pakarbi.cloudfunctions.net/loginpakarbiemail`
+export let URLLoginEmail = `https://asia-southeast2-pakarbi.cloudfunctions.net/loginnewemail`
 
 export let token = 'token';
