e7f0a6a30a845d51aa7d5f72920c82c0a2d6d155
Farhan Rizki <farhanrizki101010@gmail.com>
2023-12-09 05:27:32 +0700

    update get parkiran

--- js/controller/controllerGetParkiran.js
+++ js/controller/controllerGetParkiran.js
@@ -1,35 +1,54 @@
-// Function to make the API request with the token
-async function getUserWithToken() {
-    const token = getTokenFromCookies('Login'); // Get the token dari cookies via parameter
+const getTokenFromCookies = (cookieName) => {
+    const cookies = document.cookie.split(';')
+    for (const cookie of cookies) {
+        const [name, value] = cookie.trim().split('=')
+        if (name === cookieName) {
+            return value
+        }
+    }
+    return null
+}
+
+const getAllParkiran = async () => {
+    const token = getTokenFromCookies('Login')
 
     if (!token) {
-        alert("token tidak ditemukan");
-        return;
+        Swal.fire({
+            icon: 'warning',
+            title: 'Authentication Error',
+            text: 'You are not logged in.',
+        }).then(() => {
+            window.location.href = 'login.html'
+        })
+        return
     }
 
-    const targetURL = 'https://asia-southeast2-pakarbi.cloudfunctions.net/getallparkiran';
+    const targetURL = 'https://asia-southeast2-pakarbi.cloudfunctions.net/getallparkiran'
 
-    // Set up headers with the token
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
+    const myHeaders = new Headers()
+    myHeaders.append('Login', token)
 
     const requestOptions = {
         method: 'GET',
         headers: myHeaders,
-        redirect: 'follow'
-    };
+        redirect: 'follow',
+    }
 
     try {
-        const response = await fetch(targetURL, requestOptions);
-        const data = await response.json();
+        const response = await fetch(targetURL, requestOptions)
+        const data = await response.json()
 
         if (data.status === true) {
-            displayUserData(data.data);
+            displayParkiranData(data.data, 'parkiranDataBody')
         } else {
-            alert(data.message);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: data.message,
+            })
         }
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error:', error)
     }
 }
 
@@ -97,43 +116,43 @@ const deleteParkiranHandler = (ParkiranId) => {
     })
 }
 
-// Function to extract the token from cookies
-function getTokenFromCookies(cookieName) {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        if (name === cookieName) {
-            return value;
-        }
+// Event listener to handle clicks on the table
+document.getElementById('parkiranDataBody').addEventListener('click', (event) => {
+    const target = event.target
+    if (target.classList.contains('delete-link')) {
+        const ParkiranId = target.getAttribute('data-nomorid')
+        deleteParkiranHandler(ParkiranId)
     }
-    return null;
-}
+})
 
-// Function to display user data in the table
-function displayUserData(parkiranData) {
-    const parkiranDataBody = document.getElementById('parkiranDataBody');
+const displayParkiranData = (parkiranData, tableBodyId) => {
+    const parkirdatabody = document.getElementById(tableBodyId)
+
+    parkirdatabody.innerHTML = ''
 
     if (parkiranData && parkiranData.length > 0) {
-        parkiranData.forEach(parkiran => {
-            const newRow = document.createElement('tr');
+        parkiranData.forEach((item) => {
+            const newRow = document.createElement('tr')
             newRow.innerHTML = `
-                <td>${parkiran.parkiranid}</td>
-                <td>${parkiran.nama}</td>
-                <td>${parkiran.npm}</td>
-                <td>${parkiran.prodi}</td>
-                <td>${parkiran.namakendaraan}</td>
-                <td>${parkiran.nomorkendaraan}</td>
-                <td>${parkiran.jeniskendaraan}</td>
-                <td>${parkiran.status ? 'Sedang Parkir' : 'Sedang Keluar'}</td>
-                <td>
-                    <a href="#" class="delete-link" data-nomorid="${parkiran.parkiranid}">Delete</a>
-                </td>
-            `;
-            parkiranDataBody.appendChild(newRow);
-        });
+            <td>${parkiran.parkiranid}</td>
+            <td>${parkiran.nama}</td>
+            <td>${parkiran.npm}</td>
+            <td>${parkiran.prodi}</td>
+            <td>${parkiran.namakendaraan}</td>
+            <td>${parkiran.nomorkendaraan}</td>
+            <td>${parkiran.jeniskendaraan}</td>
+            <td>${parkiran.status ? 'Sedang Parkir' : 'Sedang Keluar'}</td>
+            <td>
+                <a href="#" class="delete-link" data-nomorid="${parkiran.parkiranid}">Delete</a>
+            </td>
+        `
+
+            parkirdatabody.appendChild(newRow)
+        })
     } else {
-        parkiranDataBody.innerHTML = '<tr><td colspan="3">No user data found.</td></tr>';
+        parkirdatabody.innerHTML = `<tr><td colspan="6">No parkiran data found.</td></tr>`
     }
 }
 
-getUserWithToken();
+// Initial fetch of all parkiran
+getAllParkiran()
