f048cb26bf3b6f13d64e8580ecf2f4e0003368bd
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2024-01-04 00:01:47 +0700

    edited

--- js/template/template.js
+++ js/template/template.js
@@ -1,12 +1,12 @@
 // template js
 
 //url register
-export let URLPost = `https://asia-southeast2-pakarbi.cloudfunctions.net/registerpakarbi`
+export let URLPost = `https://asia-southeast2-project3-403614.cloudfunctions.net/Register`
 
 //url login 
-export let URLLogin = `https://asia-southeast2-pakarbi.cloudfunctions.net/loginpakarbinpm`
+export let URLLogin = `https://asia-southeast2-project3-403614.cloudfunctions.net/login`
 
 //url login 
-export let URLLoginEmail = `https://asia-southeast2-pakarbi.cloudfunctions.net/loginpakarbiemail`
+export let URLLoginEmail = `https://asia-southeast2-project3-403614.cloudfunctions.net/loginUserEmail`
 
 export let token = 'token';
